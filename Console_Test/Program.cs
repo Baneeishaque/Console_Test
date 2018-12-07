ceeedc7d79b880bb087a8ce5d77c6595829b4804
Banee Ishaque K <Baneeishaque@gmail.com>
2018-12-07 18:05:45 +0530

    Add project files.

---
+++ Console_Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hai");
+        }
+    }
+}
